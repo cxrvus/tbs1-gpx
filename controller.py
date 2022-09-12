238d5e04f29e8a0d40392ea87393144d78e8f470
Benito Rabe <benito.m.rabe@gmail.com>
2022-09-12 14:07:32 +0200

    controller and parser progress

--- controller.py
+++ controller.py
@@ -1,4 +1,9 @@
 from parser import parse_all_file_names
+import db
 
 GPXDIR = './testdata'
-print(parse_all_file_names(GPXDIR))
+parsed_file_names = parse_all_file_names(GPXDIR)
+
+for file in parsed_file_names:
+	db.amend_record('fahrer', 'name', file['name'])
+	db.amend_record('fahrzeig', 'polkz', file['polkz'])
--- parser.py
+++ parser.py
@@ -1,12 +1,18 @@
 import re
 from os import listdir
+from gpxpy import parse as parse_gpx
+
+def parse_file(filename):
+	with open(filename, 'r') as file:
+		gpx = parse_gpx(file)
+		return gpx
 
 def parse_all_file_names(dir):
-	file_name_data = []
-	file_names = listdir(dir)
-	for file_name in file_names:
-		file_name_data.append(_parse_file_name(file_name))
-	return file_name_data
+	filename_data = []
+	filenames = listdir(dir)
+	for file_name in filenames:
+		filename_data.append(_parse_file_name(file_name))
+	return filename_data
 
 _gpx_file_pattern = re.compile(r'^(?P<name>[a-z]{1,32})_(?P<polkz>[a-z]{1,3}-?[a-z]{2}\d{1,4})_(?P<index>\d{3})\.gpx$', flags=re.I)
 
