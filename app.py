e82ea411260bf193ec3a3d9e757f3ebff601ea1c
Benito Rabe <benito.m.rabe@gmail.com>
2022-11-16 17:59:57 +0100

    misc

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
+import flask
+app = flask.Flask(__name__)
+
+
+site = """
+<html>
+<head>
+<script>
+alert('lolxd')
+</script>
+</head>
+<body>
+<div id="my_div" style="height:100px;width:100px;background-color:green"></div>
+<button onclick="my_div.style.backgroundColor='blue'">BRUH</button>
+</body>
+</html>
+"""
+
 
 @app.route('/')
 def render_html():
-    return '<p> Hallo </p>'
+    return site
