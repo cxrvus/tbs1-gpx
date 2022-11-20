9505b43529406aed24429919b9d99bf1951ffc60
Benito Rabe <benito.m.rabe@gmail.com>
2022-11-20 21:21:23 +0100

    implemented drawing paths on map

--- static/index.js
+++ static/index.js
@@ -1,4 +1,22 @@
+const drawTrack = track => {
+    const coordinates = track.points.map(p => [p.lat.toFixed(5), p.lon.toFixed(5)]);
+    const polyline = L.polyline(coordinates,
+		{
+			weight: 6,
+			color: 'darkred'
+		}
+		).addTo(window.mymap);
+    mymap.fitBounds(polyline.getBounds());
+}
+
 const init = () => {
+	window.mymap = L.map('map');
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		maxZoom: 50
+	}).addTo(window.mymap);
+
 	window.fahrtSelect = document.getElementById('fahrtSelect')
 	listTracks()
 }
@@ -26,5 +44,5 @@ const listTracks = () => {
 
 const renderPath = () => {
 	const chosenTrack = tracks[parseInt(fahrtSelect.value)]
-	console.log(chosenTrack)
+	drawTrack(chosenTrack)
 }
